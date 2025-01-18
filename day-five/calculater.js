68f680c1d0331c43e13df4bbf187a8a758c38592
David Heidari <davidheidari2@gmail.com>
2025-01-18 16:09:51 +0100

    Done with the project, need to fix some minor issus only

--- day-five/calculater.js
+++ day-five/calculater.js
@@ -17,6 +17,7 @@ function divideNum(num1, num2) {
   return num1 / num2;
 }
 
+// Kollar på värdet av operator och skickar med num1 och num2
 function operate(operator, num1, num2) {
   switch (operator) {
     case "+":
@@ -43,7 +44,7 @@ let decimal = false;
 
 function setNumbers(num) {
   if (operator === "") {
-    num1 += num;
+    num1 += num; // konkatering: det första stränget + det andra: "2" + "1" = "21"
   } else {
     num2 += num;
   }
@@ -85,19 +86,15 @@ function checkDecimal() {
 function backSpace() {
   if (!operator && num1) {
     display.textContent = display.textContent.toString().slice(0, -1);
-  } else if (op && !num2) {
+    num1 = num1.slice(0, -1);
+  } else if (operator && !num2) {
     display.textContent = display.textContent.toString().slice(0, -1);
-  } else if (op && num2) {
+    operator = "";
+  } else if (operator && num2) {
     display.textContent = display.textContent.toString().slice(0, -1);
+    num2 = num2.slice(0, -1);
   }
 }
-
-console.log(operate(addNum, 10, 5));
-console.log(operate(substractNum, 10, 5));
-console.log(operate(multiplyNum, 10, 5));
-console.log(operate(divideNum, 10, 5));
-
-console.log(addNum(1, 3));
 
 // -----------------* Calculator Project ----------------* //
 const container = document.querySelector(".container");
@@ -125,7 +122,104 @@ const seven = document.getElementById("seven");
 const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
 
+// Symbols even
+
+add.addEventListener("click", () => {
+  if (checkOprator()) {
+    display.textContent += "+";
+    setOperator("+");
+  }
+});
+
+substract.addEventListener("click", () => {
+  if (checkOprator()) {
+    display.textContent += "-";
+    setOperator("-");
+  }
+});
+
+multiply.addEventListener("click", () => {
+  if (checkOprator()) {
+    display.textContent += "*";
+    setOperator("*");
+  }
+});
+
+divide.addEventListener("click", () => {
+  if (checkOprator()) {
+    display.textContent += "/";
+    setOperator("/");
+  }
+});
+
+equal.addEventListener("click", () => {
+  if (check()) {
+    let answer = operate(operator, num1, num2);
+    display.textContent = answer;
+    num1 = answer;
+    num2 = "";
+    operator = "";
+  }
+});
+
+dot.addEventListener("click", checkDecimal);
+
+backspace.addEventListener("click", backSpace);
+
+clear.addEventListener("click", () => {
+  display.textContent = "";
+  num1 = "";
+  num2 = "";
+  operator = "";
+  decimal = false;
+});
+
 zero.addEventListener("click", () => {
   display.textContent += "0";
-  setOperator(0);
+  setNumbers(0);
+});
+
+one.addEventListener("click", () => {
+  display.textContent += "1";
+  setNumbers(1);
+});
+
+two.addEventListener("click", () => {
+  display.textContent += "2";
+  setNumbers(2);
+});
+
+three.addEventListener("click", () => {
+  display.textContent += "3";
+  setNumbers(3);
+});
+
+four.addEventListener("click", () => {
+  display.textContent += "4";
+  setNumbers(4);
+});
+
+five.addEventListener("click", () => {
+  display.textContent += "5";
+  setNumbers(5);
+});
+
+six.addEventListener("click", () => {
+  display.textContent += "6";
+  setNumbers(6);
+});
+
+seven.addEventListener("click", () => {
+  display.textContent += "7";
+  setNumbers(7);
+});
+
+eight.addEventListener("click", () => {
+  display.textContent += "8";
+  setNumbers(8);
+});
+
+nine.addEventListener("click", () => {
+  display.textContent += "9";
+  setNumbers(9);
 });
